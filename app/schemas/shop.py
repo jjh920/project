3c98244f9be53f6baf014c9d1aaab1d36926eba5
YeEul, Kwon <gruuriii@gmail.com>
2024-02-08 18:35:19 +0900

    cart, product markup, css ..ing

---
+++ app/schemas/shop.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Cart(BaseModel):
+    pass
+
+
+class NewCart(BaseModel):
+    pass
---
+++ app/services/shop.py
@@ -0,0 +1,8 @@
+from sqlalchemy import insert, select, update, func
+
+from app.dbfactory import Session
+from app.models.shop import Cart
+
+
+class CartService():
+    pass
