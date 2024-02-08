4226ba84321a3937595799478392cf134cc54905
YeEul, Kwon <gruuriii@gmail.com>
2024-02-08 16:33:21 +0900

    main에 shop_router 추가

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from app.dbfactory import db_startup
+from app.routes.board import board_router
+from app.routes.member import member_router
+from app.routes.shop import shop_router
+
+app = FastAPI()
+
+
+# jinja2 설정
+templates = Jinja2Templates(directory='views/templates')
+app.mount('/static', StaticFiles(directory='views/static'), name='static')
+
+
+# 외부 라우터 불러오기
+app.include_router(shop_router, prefix='/shops')
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(req: Request):
+    return templates.TemplateResponse(
+        'index.html', {'request': req})
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
